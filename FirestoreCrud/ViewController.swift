3d282b320dbbe8ed2cfb9fe53b7e69b3daa8887c
Kiara Itzel Laredo González <kiara_itzel@hotmail.com>
2022-03-02 17:12:40 -0600

    feat: show data funcionality

--- FirestoreCrud/ViewController.swift
+++ FirestoreCrud/ViewController.swift
@@ -6,26 +6,72 @@
 //
 
 import UIKit
+import FirebaseFirestore
+
+struct Peliculas {
+    var titulo: String
+    var genero: String
+    var id: String
+}
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tabla: UITableView!
+    
+    var db: Firestore!
+    var lista = [Peliculas]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tabla.delegate = self
         tabla.dataSource = self
+        db = Firestore.firestore()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        traerDatos()
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return lista.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tabla.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "hola"
-        cell.detailTextLabel?.text = "mundo"
+        let pelicula = lista[indexPath.row]
+        cell.textLabel?.text = pelicula.titulo
+        cell.detailTextLabel?.text = pelicula.genero
         return cell
     }
 
+    func traerDatos(){
+        db.collection("peliculas").getDocuments { (querySnapshot,error) in
+            if let error = error {
+                print("Error al traer los datos", error.localizedDescription)
+            }else{
+                self.lista.removeAll()
+                for document in querySnapshot!.documents{
+                    let valores = document.data()
+                    let id = document.documentID
+                    let titulo = valores["titulo"] as? String ?? "Sin titulo"
+                    let genero = valores["genero"] as? String ?? "Sin genero"
+                    let pelicula = Peliculas(titulo: titulo, genero: genero, id: id)
+                    self.lista.append(pelicula)
+                    DispatchQueue.main.async {
+                        self.tabla.reloadData()
+                    }
+                }
+            }
+        }
+    }
+    
+    func traerDatosRealTime(){
+        
+    }
+    
+    func traerDatosWhere(){
+        
+    }
 }
 
